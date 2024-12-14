44079d2cc00d7804f7c15e95cd5b9bb6aa43563b
hakhddhujsjdyw <manafalthaf01@gmail.com>
2024-12-14 14:23:11 +0530

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,57 @@
+# (c) @RknDeveloperr
+# Rkn Developer 
+# Don't Remove Credit 😔
+# Telegram Channel @RknDeveloper & @Rkn_Bots
+# Developer @RknDeveloperr
+
+from aiohttp import web
+from pyrogram import Client
+from config import Rkn_Bots, Rkn_Bots as Rkn_Botz
+from Rkn_Bots.web_support import web_server
+
+class Rkn_AutoCaptionBot(Client):
+    def __init__(self):
+        super().__init__(
+            name="Rkn-Advance-Caption-Bot",
+            api_id=Rkn_Bots.API_ID,
+            api_hash=Rkn_Bots.API_HASH,
+            bot_token=Rkn_Bots.BOT_TOKEN,
+            workers=200,
+            plugins={"root": "Rkn_Bots"},
+            sleep_threshold=15,
+        )
+
+    async def start(self):
+        await super().start()
+        me = await self.get_me()
+        self.uptime = Rkn_Botz.BOT_UPTIME
+        self.force_channel = Rkn_Bots.FORCE_SUB
+        if Rkn_Bots.FORCE_SUB:
+            try:
+                link = await self.export_chat_invite_link(Rkn_Bots.FORCE_SUB)
+                self.invitelink = link
+            except Exception as e:
+                print(e)
+                print("Make Sure Bot admin in force sub channel")
+                self.force_channel = None
+        app = web.AppRunner(await web_server())
+        await app.setup()
+        bind_address = "0.0.0.0"
+        await web.TCPSite(app, bind_address, Rkn_Bots.PORT).start()
+        print(f"{me.first_name} Iꜱ Sᴛᴀʀᴛᴇᴅ.....✨️")
+        for id in Rkn_Bots.ADMIN:
+            try:
+                await self.send_message(id, f"**__{me.first_name}  Iꜱ Sᴛᴀʀᴛᴇᴅ.....✨️__**")
+            except:
+                pass
+        
+    async def stop(self, *args):
+        await super().stop()
+        print("Bot Stopped 🙄")
+        
+Rkn_AutoCaptionBot().run()
+
+# Rkn Developer 
+# Don't Remove Credit 😔
+# Telegram Channel @RknDeveloper & @Rkn_Bots
+# Developer @RknDeveloperr
